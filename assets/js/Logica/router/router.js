b2da1f5ab4addbcdd3b07c73c7bf22ee641f39b5
kamiloPROO <fotocopiasa100@gmail.com>
2022-12-03 12:55:52 -0500

    no se que se hizo

--- assets/js/Logica/router/router.js
+++ assets/js/Logica/router/router.js
@@ -1,14 +1,27 @@
 const URL_API_BASE ="http://52.175.225.67:63202";
+const URL_API_BASE_PERSONAS ="https://apiamingenieria.app/";
 const URL_API_BASE_REGISTRO = "http://52.175.225.67:8082/api/persona";
 const URL_API_BASE_LOGIN = "http://52.175.225.67:8082/api/persona";
 const URL_API_BASE_VERIFICACION = "http://52.175.225.67:8082/api/persona";
 
-const URL_API_BASE_AGREGAR_COMERCIO = "http://52.175.225.67:63202/api/Comercios";
+const URL_API_BASE_AGREGAR_COMERCIO = "https://myqlub.app/api/Comercios";
 
 
 
-const URL_API_REGISTRO = URL_API_BASE_REGISTRO + "/CrearPersona";
-const URL_API_LOGIN = URL_API_BASE_LOGIN + "/IniciarSesion";
-const URL_API_VERIFICACION = URL_API_BASE_VERIFICACION + "/VerificarCodigo";
+// const URL_API_REGISTRO = URL_API_BASE_REGISTRO + "/CrearPersona";
+// const URL_API_LOGIN = URL_API_BASE_LOGIN + "/IniciarSesion";
+// const URL_API_VERIFICACION = URL_API_BASE_VERIFICACION + "/VerificarCodigo";
 
 const URL_API_AGREGAR_COMERCIO = URL_API_BASE_AGREGAR_COMERCIO + "/StoreNewTrade";
+
+
+// const URL_API_BASE_PERSONAS ="https://apiamingenieria.app/";
+// const URL_API_BASE_REGISTRO = "https://52.175.225.67:8082/api/persona";
+// const URL_API_BASE_LOGIN = "http://52.175.225.67:8082/api/persona";
+// const URL_API_BASE_VERIFICACION = "http://52.175.225.67:8082/api/persona";
+
+
+
+const URL_API_REGISTRO = URL_API_BASE_PERSONAS + "api/persona/CrearPersona";
+const URL_API_LOGIN = URL_API_BASE_PERSONAS + "api/persona/IniciarSesion";
+const URL_API_VERIFICACION = URL_API_BASE_PERSONAS + "api/persona/VerificarCodigo";
