75856ee7e05b64c77da6d6d2846572a20abd0065
kamiloPROO <fotocopiasa100@gmail.com>
2022-12-03 13:01:49 -0500

    rutas seguras

--- assets/js/Logica/router/router.js
+++ assets/js/Logica/router/router.js
@@ -1,4 +1,4 @@
-const URL_API_BASE ="http://52.175.225.67:63202";
+const URL_API_BASE ="https://myqlub.app";
 const URL_API_BASE_PERSONAS ="https://apiamingenieria.app/";
 const URL_API_BASE_REGISTRO = "http://52.175.225.67:8082/api/persona";
 const URL_API_BASE_LOGIN = "http://52.175.225.67:8082/api/persona";
