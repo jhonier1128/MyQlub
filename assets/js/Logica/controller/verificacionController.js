7d37158da8acf641db32dcf1a0427db1cbdcbdaf
kamiloPROO <fotocopiasa100@gmail.com>
2022-12-06 15:03:37 -0500

    login melo

--- assets/js/Logica/controller/verificacionController.js
+++ assets/js/Logica/controller/verificacionController.js
@@ -15,21 +15,3 @@ function verificacionPersona() {
     verificacion(verificacionPersona)
 
 }
-
-// function verificacionPersonaSend() {
-
-//     var loginn = localStorage.getItem("phoneUserRecover")
-//     var IdUser = localStorage.getItem("IdUserRegis")
-
-//     const verificacionPersona = {
-
-//         IdPerson: IdUser,
-//         IdProject: "6",
-//         CodeVerification: $("#codigoUserVeri").val(),
-//         Login: loginn,
-//         IsRecoverPassword: "true"
-       
-//     }
-//     verificacion(verificacionPersona)
-
-// }
