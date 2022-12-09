b9185f4b9d1b88c47908e1a013c27fd581529d6c
kamiloPROO <fotocopiasa100@gmail.com>
2022-12-09 18:25:00 -0500

    recuperar contra

--- assets/js/Logica/utils/HttpsRecoveryPassword.js
+++ assets/js/Logica/utils/HttpsRecoveryPassword.js
@@ -6,32 +6,41 @@ function CallEndPointPost(url, jsonArgs) {
         data: jsonArgs,
         success: function (data) {
 
-                 const dataData = data.Data;
+            const dataData = data.Data;
 
-                console.log(data);
+            console.log(data);
 
-                 const idper = dataData.IdPerson;
+            const idper = dataData.IdPerson;
 
-                if (data.CodeResponse === 200 ){
+            if (data.CodeResponse === 200) {
 
-                    localStorage.setItem("idPersonaQlub",idper);
-                    window.location.href = URL_API_BASE;
-                    location.replace("indexLogin.html");
-                    location.href ="indexLogin.html";
+                localStorage.setItem("idPersonaQlub", idper);
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Hemos cambiado tu contraseña con exito revisa tu correo...'
 
-                }else if (data.CodeResponse != 200 ){
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = URL_API_BASE;
+                        location.replace("indexLogin.html");
+                        location.href = "indexLogin.html";
+                    }
+                })
 
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Oops...',
-                        text: 'No pudimos crear tus nuevas credenciales',
-                      })
 
-                }
-                
+            } else if (data.CodeResponse != 200) {
+
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'No pudimos crear tus nuevas credenciales',
+                })
+
+            }
+
         },
         error: function () {
-            alert("Ha ocurrido un error, por favor vuelva a intentarlo")            
+            alert("Ha ocurrido un error, por favor vuelva a intentarlo")
         }
     });
 }
--- assets/js/Logica/utils/HttpsSendCode.js
+++ assets/js/Logica/utils/HttpsSendCode.js
@@ -4,36 +4,47 @@ function CallEndPointPost(url, jsonArgs) {
         url: url,
         data: jsonArgs,
         success: function (data) {
-                
-                 console.log(data);
 
-                 const response = data.Data;
+            console.log(data);
+
+            const response = data.Data;
 
 
-                if (data.CodeResponse == 200 ){
+            if (data.CodeResponse == 200) {
 
-                    const PhoneRecover = response.Phone;
-                    const IdUserRecover = response.IdPerson;
+                const PhoneRecover = response.Phone;
+                const IdUserRecover = response.IdPerson;
 
-                    localStorage.setItem("phoneUserRecover", PhoneRecover);
-                    localStorage.setItem("IdUserRecovery", IdUserRecover);
+                localStorage.setItem("phoneUserRecover", PhoneRecover);
+                localStorage.setItem("IdUserRecovery", IdUserRecover);
 
-                    window.location.href = URL_API_BASE;
-                    location.replace("recoveryVerification.html");
-                    location.href ="recoveryVerification.html";
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Te hemos enviado un codigo de verificacion a tu numero de celular por favor revisalo'
 
-                }else if (data.CodeResponse == 409) {
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = URL_API_BASE;
+                        location.replace("recoveryVerification.html");
+                        location.href ="recoveryVerification.html"; 
+                    }
+                })
+                // window.location.href = URL_API_BASE;
+                // location.replace("recoveryVerification.html");
+                // location.href ="recoveryVerification.html";
 
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Oops...',
-                        text: 'NO te encuentras registrado en nuestro sistema',
-                    })
+            } else if (data.CodeResponse == 409) {
 
-                }
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'NO te encuentras registrado en nuestro sistema',
+                })
+
+            }
         },
         error: function () {
-            alert("Ha ocurrido un error, por favor vuelva a intentarlo")            
+            alert("Ha ocurrido un error, por favor vuelva a intentarlo")
         }
     });
 }
